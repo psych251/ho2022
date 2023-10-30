32c883837a7a085038d693556d02357f26ff3eb4
justintheyang <justintheyang@gmail.com>
2023-10-29 22:28:33 -0700

    ugh

--- experiment.psiturkapp/bin/env_to_config.py
+++ experiment.psiturkapp/bin/env_to_config.py
@@ -1,20 +1,33 @@
 import configparser
 import os
 
-
 def copy_env_to_config():
-    c = configparser.ConfigParser()
-    c.read('config.txt')
+    try:
+        # Print debugging information
+        print("Current working directory:", os.getcwd())
+        print("DATABASE_URL from environment:", os.environ.get('DATABASE_URL'))
+        print("PORT from environment:", os.environ.get('PORT'))
 
-    changed = False
-    if 'DATABASE_URL' in os.environ:
-        c['Database Parameters']['database_url'] = os.environ['DATABASE_URL']
-        changed = True
-    if 'PORT' in os.environ:
-        c['Server Parameters']['port'] = os.environ['PORT']
-        changed = True
+        c = configparser.ConfigParser()
+        c.read('config.txt')
 
-    if changed:
-        with open('config.txt', 'w') as out:
-            c.write(out)
+        changed = False
+        if 'DATABASE_URL' in os.environ:
+            c['Database Parameters']['database_url'] = os.environ['DATABASE_URL']
+            changed = True
+        if 'PORT' in os.environ:
+            c['Server Parameters']['port'] = os.environ['PORT']
+            changed = True
+
+        if changed:
+            # Print a debugging message before writing
+            print("Writing changes to config.txt")
+            with open('config.txt', 'w') as out:
+                c.write(out)
+            print("Changes written to config.txt successfully!")
+        else:
+            print("No changes detected for config.txt")
+
+    except Exception as e:
+        print("Error updating config.txt:", e)
 
